3f463de98d5b1e1620532e2cfb5ff6f5c9903e48
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-04-27 17:14:50 +0900

    fixed format

--- src/main/kotlin/rip/deadcode/michishio/main.kt
+++ src/main/kotlin/rip/deadcode/michishio/main.kt
@@ -12,13 +12,11 @@ fun main(args: Array<String>) {
     val fs = Toolbox[FileSystem::class]
 
     val sourcePath = fs.getPath(config.input)
-    val out =
-        Files.newInputStream(sourcePath).use {
-            performance {
-                compile(it)
-            }
+    val out = Files.newInputStream(sourcePath).use {
+        performance {
+            compile(it)
         }
-
+    }
 
     val destination = fs.getPath(config.output)
     Files.write(destination, out)
